253708774aae672248197a0d13750f91288e3618
Isaiah Muchene <imuchene@msn.com>
2026-01-02 17:25:57 +0300

    - Added a route for fetching all groups

--- routes/group-routes.ts
+++ routes/group-routes.ts
@@ -1,14 +1,10 @@
 import { Response, Router, Request } from 'express';
-import { User } from '../models/user.model';
-import jwt from 'jsonwebtoken';
-import { Types } from 'mongoose';
-import { CookieNames } from '../enums/cookie-names.enum';
 import { Group } from '../models/group.model';
 import { isAdmin, protect } from '../middleware/auth.middleware';
 
 export const groupRouter = Router();
 
-// Registration route
+// Create a group
 groupRouter.post('/', protect, isAdmin, async (req: Request, res: Response) => {
   try {
     const { name, description } = req.body;
@@ -43,39 +39,16 @@ groupRouter.post('/', protect, isAdmin, async (req: Request, res: Response) => {
   }
 });
 
-// Login route
-groupRouter.post('/login', async (req: Request, res: Response) => {
+// Get all groups
+groupRouter.get('/', protect, async (req: Request, res: Response) => {
   try {
-    const { email, password } = req.body;
-    const user = await User.findOne({ email });
-    if (user && (await user.matchPassword(password))) {
-      res
-        .cookie(CookieNames.AuthCookie, generateToken(user._id), {
-          signed: true,
-          secure: true,
-          httpOnly: true,
-          sameSite: 'lax',
-          expires: new Date(Date.now() + 900000), // Set the cookie to expire in 15 minutes
-        })
-        .json({
-          user: {
-            _id: user._id,
-            username: user.username,
-            email: user.email,
-            isAdmin: user.isAdmin,
-          },
-        });
-    } else {
-      res.status(401).json({ message: 'Invalid email or password' });
-    }
+    const groups = await Group.find()
+      .populate('admin', 'username email')
+      .populate('members', 'username email');
+    res.json(groups);
   } catch (error) {
     if (error instanceof Error) {
       res.status(400).json({ message: error.message });
     }
   }
 });
-
-const generateToken = (id: Types.ObjectId) => {
-  // Set the jwt token to expire in 10 minutes
-  return jwt.sign({ id }, String(process.env.JWT_SECRET), { expiresIn: '10m' });
-};
